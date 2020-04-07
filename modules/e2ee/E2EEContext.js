2ceefa9c502de81cf085bdd42d1b763b4d2cd76b
Philipp Hancke <fippo@jitsi.org>
2020-04-07 13:15:19 +0200

    cleanup some things. add some things. nothing to see here yet, carry on

--- modules/e2ee/E2EEContext.js
+++ modules/e2ee/E2EEContext.js
@@ -4,9 +4,29 @@ import { getLogger } from 'jitsi-meet-logger';
 
 const logger = getLogger(__filename);
 
+// We use a ringbuffer of keys so we can change them and still decode packets that were
+// encrypted with an old key.
+const keyRingSize = 8;
+
+// We use a 96 bit IV for AES GCM. This is signalled in plain together with the
+// packet. See https://developer.mozilla.org/en-US/docs/Web/API/AesGcmParams
+const ivLength = 12;
+
+// We copy the first bytes of the VP8 payload unencrypted.
+// For keyframes this is 10 bytes, for non-keyframes (delta) 3. See
+//   https://tools.ietf.org/html/rfc6386#section-9.1
+// This allows the bridge to continue detecting keyframes (only one byte needed in the JVB)
+// and is also a bit easier for the VP8 decoder (i.e. it generates funny garbage pictures
+// instead of being unable to decode).
+// This is a bit for show and we might want to reduce to 1 unconditionally in the final version.
+const unencryptedBytes = {
+    key: 10,
+    delta: 3
+};
+
 
 /**
- * Context encapsulating the crypt required for E2EE.s
+ * Context encapsulating the crypt required for E2EE.
  */
 export default class E2EEcontext {
 
@@ -15,8 +35,18 @@ export default class E2EEcontext {
      *
      * @param {string} options.salt - Salt to be used for key deviation.
      */
-    consructor(options) {
+    constructor(options) {
         this._options = options;
+
+        // An array (ring) of keys that we use for sending and receiving.
+        this._cryptoKeyRing = new Array(keyRingSize);
+
+        // A pointer to the currently used key.
+        this._currentKeyIndex = -1;
+
+        // We keep track of how many frames we have sent per ssrc.
+        // Starts with a random offset similar to the RTP sequence number.
+        this._sendCounts = new Map();
     }
 
     /**
@@ -30,7 +60,7 @@ export default class E2EEcontext {
         const receiverStreams
             = kind === 'video' ? receiver.createEncodedVideoStreams() : receiver.createEncodedAudioStreams();
         const transform = new TransformStream({
-            transform: this._decodeFunction
+            transform: this._decodeFunction.bind(this)
         });
 
         receiverStreams.readableStream
@@ -49,7 +79,7 @@ export default class E2EEcontext {
         const senderStreams
             = kind === 'video' ? sender.createEncodedVideoStreams() : sender.createEncodedAudioStreams();
         const transform = new TransformStream({
-            transform: this._encodeFunction
+            transform: this._encodeFunction.bind(this)
         });
 
         senderStreams.readableStream
@@ -86,16 +116,5 @@ export default class E2EEcontext {
      */
     _decodeFunction(chunk, controller) {
         // TODO.
-    }
-
-    /**
-     * Derives a key from the passed value.
-     *
-     * @param {string} value - Value to derive the key from.
-     */
-    async _deriveKey(value) {
-        // TODO.
-
-        return value;
     }
 }
